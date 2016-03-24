ca0bd831918f7b445bb904c1860ee37b5815c269
Achraf <elamraniachraf93@gmail.com>
2016-03-24 14:12:03 +0000

    découpage qui marche avec un pom parent

--- Artifacts/Cuisine/src/main/java/fr/unice/polytech/devops/components/KitchenBean.java
+++ Artifacts/Cuisine/src/main/java/fr/unice/polytech/devops/components/KitchenBean.java
@@ -13,23 +13,23 @@ import javax.ejb.Stateless;
 @Stateless
 public class KitchenBean implements OrderProcessing, Tracker {
 
-	@EJB private Database memory;
+    @EJB private Database memory;
 
-	@Override
-	public void process(Order order) {
-		if (order.getCustomer().getName().contains("p")) {
-			order.setStatus(OrderStatus.IN_PROGRESS);
-		} else if (order.getCustomer().getName().contains("r")) {
-			order.setStatus(OrderStatus.READY);
-		}
-		memory.getOrders().put(order.getId(), order);
-	}
+    @Override
+    public void process(Order order) {
+        if (order.getCustomer().getName().contains("p")) {
+            order.setStatus(OrderStatus.IN_PROGRESS);
+        } else if (order.getCustomer().getName().contains("r")) {
+            order.setStatus(OrderStatus.READY);
+        }
+        memory.getOrders().put(order.getId(), order);
+    }
 
-	@Override
-	public OrderStatus status(String orderId) throws UnknownOrderId {
-		Order order = memory.getOrders().get(orderId);
-		if (order == null)
-			throw new UnknownOrderId(orderId);
-		return  order.getStatus();
-	}
+    @Override
+    public OrderStatus status(String orderId) throws UnknownOrderId {
+        Order order = memory.getOrders().get(orderId);
+        if (order == null)
+            throw new UnknownOrderId(orderId);
+        return  order.getStatus();
+    }
 }
